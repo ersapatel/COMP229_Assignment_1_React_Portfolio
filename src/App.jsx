8ab6a3386bb7d67b9dba5096ca3017795ebf3c1f
ersapatel <ersapatel@gmail.com>
2025-09-21 10:40:02 -0400

    Routing Skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+
+import Home from './Page/home';
+import AboutMe from './Page/about-me';
+import Projects from './Page/projects';
+import Services from './Page/services';
+import Contact from './Page/contact';
 
 function App() {
 
   return (
     <>
-      <p>Content goes here</p>
+      <BrowserRouter>
+        {/* Navigation */}
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link> |{" "}
+          <Link to="/projects">Projects</Link> |{" "}
+          <Link to="/services">Services</Link> |{" "}
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Page/about-me.jsx
@@ -0,0 +1,8 @@
+export default function AboutMe() {
+    return (
+      <section>
+        <h1>About Me</h1>
+        <p>Short bio goes here…</p>
+      </section>
+    );
+  }
